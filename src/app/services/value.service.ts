7fd0fd7d3f456816d7dd7b495cd1691dbb0157b7
gacc94 <92961761+gacc94@users.noreply.github.com>
2023-07-08 00:30:56 -0500

    agregando configuracion inicial

---
+++ src/app/services/value.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {of} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ValueService {
+
+    private value = ''
+
+    constructor() {
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    setValue(value: string): void {
+        this.value = value;
+    }
+
+    getPromiseValue() {
+        return new Promise((resolve, reject) => {
+            resolve(this.value);
+        })
+    }
+
+    getObservableValue() {
+        return of(this.value);
+    }
+
+}
